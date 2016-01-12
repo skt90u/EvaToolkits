012f78b20bc751197e9d0c940686e31c4d24bca2
U-EVAAIR\Z215 <Z215@C136244.evaair.com>
2016-01-12 11:51:35 +0800

    *

--- EvaToolkits/FortifyUtils.cs
+++ EvaToolkits/FortifyUtils.cs
@@ -5,6 +5,16 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Xml.XPath;
 
 namespace EvaToolkits
 {
@@ -18,6 +28,38 @@ namespace EvaToolkits
             Sasa.IO.FilePath filePath = new Sasa.IO.FilePath(Path.Combine(paths));
 
             return filePath.ToString();
+        }
+
+        private static string SanitizeJson<T>(string value)
+        {
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+            T result = serializer.Deserialize<T>(value);
+
+            value = serializer.Serialize(result);
+
+            return value;
+        }
+
+        public static T DeserializeObject<T>(string value)
+        {
+            var result = JsonConvert.DeserializeObject<T>(SanitizeJson<T>(value));
+
+            return result;
+        }
+
+        public static string SanitizePath(string path)
+        {
+            path = HttpUtility.HtmlEncode(path);
+            StringWriter writer = new StringWriter();
+            HttpUtility.HtmlDecode(path, writer);
+            return writer.ToString();
+        }
+
+        public static string CompileXpath(string xpath)
+        {
+            XPathExpression expr = XPathExpression.Compile(xpath);
+            return expr.Expression;
         }
     }
 }
